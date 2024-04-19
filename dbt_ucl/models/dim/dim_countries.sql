4be73441a404b60f3c4c0bc8a2a5142caf6ab1ab
Kien Dang <trankien.dang@gmail.com>
2024-04-19 19:10:27 +0000

    ver 4

--- dbt_ucl/models/dim/dim_countries.sql
+++ dbt_ucl/models/dim/dim_countries.sql
@@ -1,17 +1,26 @@
 {{ config(materialized="table") }}
 
 with
-    teams as (
-        select country from {{ref("staging_teams")}}
+    teams as (select country from {{ ref("staging_teams") }}),
+    stadiums as (select country from {{ ref("staging_stadiums") }}),
+    players as (select nationality as country from {{ ref("staging_players") }}),
+    managers as (select nationality as country from {{ ref("staging_managers") }}),
+    countries_unioned as (
+        select *
+        from teams
+        union all
+        select *
+        from stadiums
+        union all
+        select *
+        from players
+        union all
+        select *
+        from managers
     ),
-    stadiums as (
-        select country from {{ref("staging_stadiums")}}
-    ),
-    players as (
-        select nationality from {{ref("staging_players")}}
-    ),
-    managers as (
-        select nationality from {{ref("staging_managers")}}
+    countries as (
+        select DISTINCT country
+        from countries_unioned
     )
-select
-    
+select row_number() over () as country_id, country as country_name
+from countries
